d1a4bc5ac3db42aabddaaec949e0190071b04a53
Storm Henry <storm.henry@redlioncanada.com>
2017-05-10 12:10:47 -0400

    Removed 'hide' class from social buttons

--- wordpress/wp-content/themes/reverie-master/partials/article/article-tag_and_share.php
+++ wordpress/wp-content/themes/reverie-master/partials/article/article-tag_and_share.php
@@ -13,12 +13,12 @@ if(!$share_copy){
     <div class='columns medium-8 large-6 medium-centered small-8 small-centered'>
         <div class='row medium-collapse'>
             <div class='columns article-share end push-6 medium-4 small-10 medium-uncentered '>
-                <span class='article-share-title hide'><?php echo $share_copy; ?></span>
-                <a href="#" class="hide" id="shareFacebook" title="Follow us on Facebook" data-share-facebook> <i class="fa fa-facebook"></i> </a>
-                <a href="#" class="hide" id="shareTwitter" title="Follow us on Twitter" class="btn btn-default" data-share-twitter data-share-text=""> <i class="fa fa-twitter"></i> </a>
-                <a href="#" class="hide" id="shareGoogle" data-share-google-plus data-share-url href="https://plus.google.com/+Cadillac" target="_blank" title="Follow us on Google+"> <i class="fa fa-google-plus"></i> </a>
-                <a href="#" class="hide" id="shareLinkedIn" href="http://cadillac.tumblr.com/" data-share-linkedin target="_blank" title="Follow us on LinkedIn"><i class="fa fa-linkedin-square"></i></a>
-                <a href="#" class="hide" id="shareEmail" target="_blank" title="Email us"> <i class="fa fa-envelope"></i> </a>
+                <span class='article-share-title'><?php echo $share_copy; ?></span>
+                <a href="#" id="shareFacebook" title="Follow us on Facebook" data-share-facebook> <i class="fa fa-facebook"></i> </a>
+                <a href="#" id="shareTwitter" title="Follow us on Twitter" class="btn btn-default" data-share-twitter data-share-text=""> <i class="fa fa-twitter"></i> </a>
+                <a href="#" id="shareGoogle" data-share-google-plus data-share-url href="https://plus.google.com/+Cadillac" target="_blank" title="Follow us on Google+"> <i class="fa fa-google-plus"></i> </a>
+                <a href="#" id="shareLinkedIn" href="http://cadillac.tumblr.com/" data-share-linkedin target="_blank" title="Follow us on LinkedIn"><i class="fa fa-linkedin-square"></i></a>
+                <a href="#" id="shareEmail" target="_blank" title="Email us"> <i class="fa fa-envelope"></i> </a>
             </div>
             <div class='columns article-tags columns pull-4 medium-6 medium-uncentered '>
                 <?php 
