70d243ceacf85c5875c6a7cd330953d5e16dc659
Storm Henry <storm.henry@redlioncanada.com>
2017-05-18 16:32:35 -0400

    Added Facebook pixel

--- wordpress/wp-content/themes/reverie-master/header.php
+++ wordpress/wp-content/themes/reverie-master/header.php
@@ -43,6 +43,21 @@
         var s_account = "gmcadillac";
         </script>
             <script src="<?php echo get_template_directory_uri(); ?>/js/s_code.js"></script>
+            <!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1546394138713580'); // Insert your pixel ID here.
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1546394138713580&ev=PageView&noscript=1"/></noscript>
+<!-- DO NOT MODIFY -->
+<!-- End Facebook Pixel Code -->
+
+<!--             Twitter pixel code for dare-greatly -->
             <script>
 !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
 },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
